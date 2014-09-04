74f614443ed255d35fccc6c168626fe3bf0a6db6
Lily <bartlett.lily@gmail.com>
2014-09-03 19:55:04 -0700

    Grey version of scorecard

---
+++ grey/greytable.php
@@ -0,0 +1,27 @@
+<html>
+	<head>
+	<title>Write 365 Scorecard</title>
+	<meta charset="UTF-8" /> 
+	<meta name="robots" content="noindex, nofollow, noarchive" />
+	<link type="text/css" rel="stylesheet" media="screen"  href="style.css" /> 
+</head>  
+
+<body>
+<div class="wrapper">
+<div class="title"><h2>Write 365 Scorecard<br> </h2></div>
+<div class="scorecard">
+	<div class="arrows" id="leftarrow"><img src="images/leftarrow.jpg" title="See the beginning of your score card!"></div>
+	<div class="box" id="dow1"><img src="images/strike.jpg" title="Great job! You wrote at least 365 words."></div>
+	<div class="box" id="dow2"><img src="images/spare.jpg" title="You wrote under 365 words."></div>
+	<div class="box" id="dow3"><img src="images/gutter.jpg" title="You didn't write any words."></div>
+	<div class="box" id="dow4"><img src="images/strike.jpg" title="Great job! You wrote at least 365 words."></div>
+	<div class="box" id="dow5"><img src="images/strike.jpg" title="Great job! You wrote at least 365 words."></div>
+	<div class="box" id="dow6"><img src="images/spare.jpg" title="You wrote under 365 words."></div>
+	<div class="box" id="dow7"><img src="images/gutter.jpg" title="You didn't write any words."></div>
+	<div class="box" id="dow8"><img src="images/strike.jpg" title="Great job! You wrote at least 365 words."></div>
+	<div class="box" id="dow9"><img src="images/strike.jpg" title="Great job! You wrote at least 365 words."></div>
+	<div class="box" id="dow10"><img src="images/spare.jpg" title="You wrote under 365 words."></div>
+	<div class="arrows" id="rightarrow"><img src="images/rightarrow.jpg" title="Go forward 10 tiles"></div>
+</div>
+</body>
+</html>
